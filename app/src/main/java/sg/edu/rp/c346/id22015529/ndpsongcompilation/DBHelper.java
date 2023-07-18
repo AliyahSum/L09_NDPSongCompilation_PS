6a4515a92d3f89cbd93aa3942c67a24d60aa50fb
AliyahSum <22015529@myrp.edu.sg>
2023-07-18 23:28:52 +0800

    insert + retrieve

--- app/src/main/java/sg/edu/rp/c346/id22015529/ndpsongcompilation/DBHelper.java
+++ app/src/main/java/sg/edu/rp/c346/id22015529/ndpsongcompilation/DBHelper.java
@@ -1,9 +1,13 @@
 package sg.edu.rp.c346.id22015529.ndpsongcompilation;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 public class DBHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "songcompilation.db" ;
@@ -23,16 +27,51 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         String createSongTableSql = "CREATE TABLE " + TABLE_SONG + "("
                 + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + COLUMN_SONG_TITLE_CONTENT + " TEXT "
-                + COLUMN_SINGER_CONTENT + " TEXT "
-                + COLUMN_YEAR_CONTENT + " TEXT "
-                + COLUMN_STARS_CONTENT + " TEXT)" ;
+                + COLUMN_SONG_TITLE_CONTENT + " TEXT, "
+                + COLUMN_SINGER_CONTENT + " TEXT, "
+                + COLUMN_YEAR_CONTENT + " INTEGER, "
+                + COLUMN_STARS_CONTENT + " INTEGER)" ;
         db.execSQL(createSongTableSql) ;
         Log.i("info", "created tables") ;
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("ALTER TABLE " + TABLE_SONG + " ADD COLUMN module_name TEXT ") ;
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_SONG);
+        onCreate(db);
+
+        //db.execSQL("ALTER TABLE " + TABLE_SONG + " ADD COLUMN module_name TEXT ") ;
     }
+
+    public void insertSong(String title, String singers, int year, int stars){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(COLUMN_SONG_TITLE_CONTENT, title);
+        values.put(COLUMN_SINGER_CONTENT, singers);
+        values.put(COLUMN_YEAR_CONTENT, year);
+        values.put(COLUMN_STARS_CONTENT, stars);
+
+        db.insert(TABLE_SONG, null, values);
+
+        db.close();
+    }
+
+    public ArrayList<String> getSongContent() {
+        ArrayList<String> songs = new ArrayList<String>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns = {COLUMN_ID, COLUMN_SONG_TITLE_CONTENT, COLUMN_SINGER_CONTENT, COLUMN_YEAR_CONTENT, COLUMN_STARS_CONTENT};
+        Cursor cursor = db.query(TABLE_SONG, columns, null, null, null, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                songs.add(cursor.getString(1));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+
+        return songs;
+    }
+
 }
--- app/src/main/java/sg/edu/rp/c346/id22015529/ndpsongcompilation/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22015529/ndpsongcompilation/MainActivity.java
@@ -3,12 +3,42 @@ package sg.edu.rp.c346.id22015529.ndpsongcompilation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etSongTitle, etSingers, etYear ;
+    RadioGroup rgStars ;
+    Button btnInsert, btnViewList ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etSongTitle = findViewById(R.id.etSongTitle) ;
+        etSingers = findViewById(R.id.etSingers) ;
+        etYear = findViewById(R.id.etYear) ;
+        rgStars = findViewById(R.id.radioGroupStars) ;
+        btnInsert = findViewById(R.id.buttonInsert) ;
+        btnViewList = findViewById(R.id.buttonShowList) ;
+
+        btnInsert.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                String title = etSongTitle.getText().toString() ;
+                String singers = etSingers.getText().toString() ;
+                int year = Integer.parseInt(etYear.getText().toString()) ;
+                int stars = rgStars.getCheckedRadioButtonId() ;
+
+                db.insertSong(title, singers, year, stars);
+            }
+        });
+
     }
 }
